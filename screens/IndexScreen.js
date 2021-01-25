b72c9be1a86a9efe0ae6d8201651803e9fbebbe2
shengrong <shengrong_88@hotmail.com>
2021-01-25 10:43:44 +0800

    added map items to read from API using axios tested & done

--- screens/IndexScreen.js
+++ screens/IndexScreen.js
@@ -1,9 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 import { commonStyles } from "../styles/commonStyles";
 import { Entypo } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import axios from "axios";
+import { FlatList } from "react-native-gesture-handler";
 
-export default function IndexScreen({ navigation }) {
+const API = "https://zzcloud88zz.pythonanywhere.com";
+const API_ALLITEMS = "/items";
+
+export default function IndexScreen({ route, navigation }) {
   // Add button on header
   useEffect(() => {
     navigation.setOptions({
@@ -20,12 +26,46 @@ export default function IndexScreen({ navigation }) {
     });
   });
 
+  // Render items
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get(API + API_ALLITEMS)
+    .then(response => {
+      console.log(response)
+      setItems(response.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [route.params?.title, route.paramas?.content])
+
+  // Return screen
   return (
-    <View style={commonStyles.container}>
-      <Text>Index Screen</Text>
+    <View>
+      {items.map(item => (
+        <TouchableOpacity key={item.id} onPress={() => navigation.navigate("", item)} style={styles.container}>
+          <Text>
+            {item.id}. {item.stock}
+          </Text>
+          <TouchableOpacity onPress={""}>
+            <AntDesign name="delete" size={30} color="maroon" />
+          </TouchableOpacity>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+});
